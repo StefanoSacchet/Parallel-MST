c3941ab036f584a8eed6d3099a7844d445633417
luiss07 <luigi.delleva@gmail.com>
2025-04-19 23:16:28 +0200

    broken with more than 2 processes

--- src/mpi/mpi_mst.c
+++ src/mpi/mpi_mst.c
@@ -52,20 +52,20 @@ void mpi_mst(struct Graph *graph, struct Graph *mst) {
 
   int edges_mst = 0;
   // Cheapest outgoing edge for each component
-  int *cheapest = (int *)malloc(n_vertices * sizeof(int));
-	int* cheapest_edge_received = (int*)malloc(n_vertices * sizeof(int));
+  Edge_t *cheapest = (Edge_t *)malloc(n_vertices * sizeof(Edge_t));
+	Edge_t* cheapest_edge_received = (Edge_t*)malloc(n_vertices * sizeof(Edge_t));
 
   // Initialize subsets and cheapest array
   for (int v = 0; v < n_vertices; v++) {
     subsets[v].parent = v;
     subsets[v].rank = 0;
-    cheapest[v] = -1;
+    cheapest[v].weight = -1;
   }
 
   for (int i = 0; i < n_vertices && edges_mst < n_vertices - 1; i++) {
     // reset cheapest edges array
     for (int j = 0; j < n_vertices; j++) {
-      cheapest[j] = -1;
+      cheapest[j].weight = -1;
     }
 
     // Traverse through all edges and update cheapest of every component
@@ -75,22 +75,14 @@ void mpi_mst(struct Graph *graph, struct Graph *mst) {
       int set2 = find(subsets, current_edge.dest);
 
       if (set1 != set2) {
-        if (cheapest[set1] == -1 || edges_part[cheapest[set1]].weight > current_edge.weight) {
-          cheapest[set1] = j;
+        if (cheapest[set1].weight == -1 || cheapest[set1].weight > current_edge.weight) {
+          cheapest[set1] = current_edge;
         }
-        if (cheapest[set2] == -1 || edges_part[cheapest[set2]].weight > current_edge.weight) {
-          cheapest[set2] = j;
+        if (cheapest[set2].weight == -1 || cheapest[set2].weight > current_edge.weight) {
+          cheapest[set2] = current_edge;
         }
       }
     }
-
-    // printf("rank: %d\n", rank);
-    // for (int k = 0; k < n_vertices; k++) {
-    //   printf("cheapest[%d] = %d\n", k, cheapest[k]);
-    //   if (cheapest[k] != -1) {
-    //     printf("[%d] src = %d | dest = %d | weight = %d\n", k, edges_part[cheapest[k]].src, edges_part[cheapest[k]].dest, edges_part[cheapest[k]].weight);  
-    //   }
-    // }
 
     int from;
     int to;
@@ -98,28 +90,28 @@ void mpi_mst(struct Graph *graph, struct Graph *mst) {
       if (rank % (2 * step) == 0) {
         from = rank + step;
         if (from < size) {
-          MPI_Recv(cheapest_edge_received, n_vertices, MPI_INT, from, 0, MPI_COMM_WORLD, &status);
+          MPI_Recv(cheapest_edge_received, n_vertices, MPI_EDGE_T, from, 0, MPI_COMM_WORLD, &status);
 
           // combine cheapest edges
           for (int j = 0; j < n_vertices; j++) {
             // int current_vertex = j;
-            if (edges_part[cheapest_edge_received[j]].weight < edges_part[cheapest[j]].weight) {
+            if (cheapest_edge_received[j].weight != -1 && (cheapest[j].weight == -1 || cheapest_edge_received[j].weight < cheapest[j].weight)) {
               cheapest[j] = cheapest_edge_received[j];
             }
           }
         }
       } else if (rank % step == 0) {
         to = rank - step;
-        MPI_Send(cheapest, n_vertices, MPI_INT, to, 0, MPI_COMM_WORLD);
+        MPI_Send(cheapest, n_vertices, MPI_EDGE_T, to, 0, MPI_COMM_WORLD);
       }
     }
-
     MPI_Bcast(cheapest, n_vertices, MPI_INT, 0, MPI_COMM_WORLD);
-    
+
     // add new edges to MST
     for (int j = 0; j < n_vertices; j++) {
-      if (cheapest[j] != -1) {
-        Edge_t edge = edges_part[cheapest[j]];
+      if (cheapest[j].weight != -1) {
+
+        Edge_t edge = cheapest[j];
         
         int from = find(subsets, edge.src);
         int to = find(subsets, edge.dest);
@@ -130,12 +122,18 @@ void mpi_mst(struct Graph *graph, struct Graph *mst) {
             printf("Edge %d-%d with weight %d included in MST\n", edge.src,
               edge.dest, edge.weight);
             }
+            edges_mst++;
           }
-          edges_mst++;
           unionSets(subsets, from, to);
       }
     }
+    MPI_Bcast(&edges_mst, 1, MPI_INT, 0, MPI_COMM_WORLD);
   }
+
+  free(edges_part);
+  free(subsets);
+  free(cheapest);
+  free(cheapest_edge_received);
 }
 
 /** @brief Run the parallel version of Boruvka algorithm using MPI
@@ -179,7 +177,7 @@ void run_mpi_mst(int argc, char *argv[]) {
   /*  exit(EXIT_FAILURE);*/
   /*}*/
 
-  // mpi_mst(graph, mst);
+  mpi_mst(graph, mst);
 
   if (rank == 0) {
     double total_time = MPI_Wtime() - start_time;

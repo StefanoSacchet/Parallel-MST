1b97972eaef764f7df6175644119fa221d132b7c
StefanoSacchet <sacchet13@gmail.com>
2025-05-15 16:00:00 +0200

    script for serial mode

---
+++ run_serial.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Check if input file is passed
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <input_file>"
+    exit 1
+fi
+
+input_file="$1"
+mkdir -p logs/
+
+source load_modules.sh
+echo "Building release SERIAL..."
+make clean
+make release-hpc RUN_TYPE=SERIAL
+
+# Create a temporary PBS script
+job_script=$(mktemp)
+
+cat <<EOF > "$job_script"
+#!/bin/bash
+#PBS -l select=1:ncpus=1:mem=32gb
+#PBS -l walltime=00:15:00
+#PBS -q short_cpuQ
+#PBS -N parallel_mst
+#PBS -o logs/mpi_parallel_mst.o1
+#PBS -e logs/mpi_parallel_mst.e1
+${PWD}/build/bin/parallel_mst "$input_file"
+EOF
+
+qsub "$job_script"

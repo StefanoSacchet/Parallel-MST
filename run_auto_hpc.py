053187823078ba8f6ff9432a73134006ee0559c7
luiss07 <luigi.delleva@gmail.com>
2025-05-25 14:45:42 +0200

    minor auto run

--- run_auto_hpc.py
+++ run_auto_hpc.py
@@ -13,7 +13,9 @@ def run_mpi(num_processes, input_file, log_folder):
     file_path=f"mpi_parallel_mst.o{num_processes}"
 
     os.system(cmd)
-    print(f"Running MPI with {num_processes}. Waiting completition...")
+
+    time = datetime.now().strftime("%H:%M")
+    print(f"{time} Running MPI with {num_processes}. Waiting completition...")
 
     wait_completition(log_folder, file_path)
 
@@ -22,7 +24,8 @@ def run_omp(num_processes, input_file, log_folder):
     file_path=f"omp_parallel_mst.o{num_processes}"
 
     os.system(cmd)
-    print(f"Running OMP with {num_processes}. Waiting completition...")
+    time = datetime.now().strftime("%H:%M")
+    print(f"{time} Running OMP with {num_processes}. Waiting completition...")
 
     wait_completition(log_folder, file_path) 
 
@@ -43,10 +46,10 @@ def run_script():
     input_file = argv[2]
 
     num_processes = 2
-    for i in range(0, run_number):
-        time = datetime.now().strftime("%d_%m_%Y@%H_%M")
-        log_folder=f"{time}/{i}_run"
-        while num_processes<=64:
+    time = datetime.now().strftime("%d_%m_%Y@%H_%M")
+    while num_processes<=64:
+        for i in range(0, run_number):
+            log_folder=f"{time}/{i}_run"
             # run script
             if mode == "mpi":
                 run_mpi(num_processes, input_file, log_folder)
@@ -55,7 +58,7 @@ def run_script():
             elif mode == "all":
                 run_all(num_processes, input_file, log_folder)
                 
-            num_processes*=2    
+        num_processes*=2    
 
 if __name__ == "__main__":
     if len(argv) < 3:

f555756603cecfd81e29a5829c75f5eac1aae51f
luiss07 <luigi.delleva@gmail.com>
2025-05-26 12:28:44 +0200

    added serial to auto run

--- run_auto_hpc.py
+++ run_auto_hpc.py
@@ -8,6 +8,18 @@ def wait_completition(log_folder, file_path):
     while not os.path.exists(path):
         sleep(20)
 
+def run_serial(input_file, log_folder):
+    cmd = f"./run_serial.sh {input_file} {log_folder}"
+    file_path="serial_parallel_mst.o1"
+
+    os.system(cmd)
+
+    time = datetime.now().strftime("%H:%M")
+    print(f"{time} Running SERIAL. Waiting completition...")
+
+    wait_completition(log_folder, file_path)
+
+
 def run_mpi(num_processes, input_file, log_folder):
     cmd = f"./run_mpi_hpc.sh {num_processes} {input_file} {log_folder}"
     file_path=f"mpi_parallel_mst.o{num_processes}"
@@ -58,11 +70,15 @@ def run_script():
             elif mode == "all":
                 run_all(num_processes, input_file, log_folder)
                 
-        num_processes*=2    
+        num_processes*=2
+ 
+    for i in range(0, run_number):
+        log_folder=f"{time}/{i}_run"
+        run_serial(input_file, log_folder)   
 
 if __name__ == "__main__":
     if len(argv) < 3:
-        print("Usage: python3 run_auto.py <[mpi, omp]> <input_file> <runs_number>")
+        print("Usage: python3 run_auto.py <[mpi, omp, all]> <input_file> <runs_number>")
         exit(1)
 
     run_script()

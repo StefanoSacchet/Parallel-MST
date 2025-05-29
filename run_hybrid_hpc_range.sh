f939a31ad96ebb25dd6fa3ea54e4d6ab8a0a6862
StefanoSacchet <sacchet13@gmail.com>
2025-05-29 16:51:05 +0200

    hpc script for hybrid

---
+++ run_hybrid_hpc_range.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# This script runs the Hybrid program on HPC with a range of processes
+
+# Check if input file is passed
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <input_file>"
+    exit 1
+fi
+
+input_file="$1"
+mkdir -p logs/
+
+source load_modules.sh
+echo "Building release Hybrid..."
+make clean
+make release-hpc RUN_TYPE=HYBRID
+
+generate_and_submit_pbs_script() {
+    local select_nodes="$1"
+    local ncpus="$2"
+    local mem="$3"
+
+    # Create a temporary PBS script
+    job_script=$(mktemp)
+
+    cat <<EOF > "$job_script"
+#!/bin/bash
+#PBS -l select=${select_nodes}:ncpus=${ncpus}:mem=${mem}
+#PBS -l walltime=00:20:00
+#PBS -q short_cpuQ
+#PBS -N parallel_mst_${ncpus}
+#PBS -o logs/mpi_parallel_mst.o${ncpus}
+#PBS -e logs/mpi_parallel_mst.e${ncpus}
+
+module load mpich-3.2
+mpirun.actual -n ${ncpus} ${PWD}/build/bin/mpi_mst "$input_file"
+EOF
+
+    echo "Submitting job with $ncpus processes on $select_nodes node(s)..."
+    qsub "$job_script"
+}
+
+for ((p=2; p<=64; p*=2)); do
+  generate_and_submit_pbs_script 1 "$p" 64gb
+done
+

e59e4d252edc66e090c33b1be4be87e80890f30a
luiss07 <luigi.delleva@gmail.com>
2025-05-29 17:55:49 +0200

    auto run new heuristics

--- run_auto_hpc.py
+++ run_auto_hpc.py
@@ -3,12 +3,14 @@ import os
 from time import sleep
 from datetime import datetime
 
-def wait_completition(log_folder):
+def wait_completition(log_folder, file_count):
     path = 'logs/' + log_folder
-    file_count = 0
+    current_files = len([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))])
+    wait_file = file_count + current_files
 
-    while file_count != 24:
-        file_count = len([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))])
+    while current_files < wait_file:
+        current_files = len([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))])
+        print(f"Found {current_files} files. Needs to reach ${wait_file}")
         sleep(40)
 
 def run_serial(input_file):
@@ -19,16 +21,16 @@ def run_serial(input_file):
     time = datetime.now().strftime("%H:%M")
     print(f"{time} Running SERIAL. Waiting completition...")
 
-def run_mpi(num_processes, input_file, log_folder):
-    cmd = f"./run_mpi_hpc.sh {num_processes} {input_file} {log_folder}"
+def run_mpi(num_processes, input_file, place, log_folder):
+    cmd = f"./run_mpi_hpc.sh {num_processes} {input_file} {place} {log_folder}"
 
     os.system(cmd)
 
     time = datetime.now().strftime("%H:%M")
     print(f"{time} Running MPI with {num_processes}. Waiting completition...")
 
-def run_omp(num_processes, input_file, log_folder):
-    cmd = f"./run_omp_hpc.sh {num_processes} {input_file} {log_folder}"
+def run_omp(num_processes, input_file, place, log_folder):
+    cmd = f"./run_omp_hpc.sh {num_processes} {input_file} {place} {log_folder}"
 
     os.system(cmd)
     time = datetime.now().strftime("%H:%M")
@@ -48,20 +50,25 @@ def run_script():
 
     num_processes = 2
     time = datetime.now().strftime("%d_%m_%Y@%H_%M")
-    for i in range(0, run_number):
-        log_folder=f"{time}/{i}_run"
-        while num_processes<=64:
-            if mode == "mpi" or mode == "all":
-                run_mpi(num_processes, input_file, log_folder)
+    for place in ["pack", "scatter"]:
+        for i in range(0, run_number):
+            log_folder=f"{time}/{i}_run"
+            file_count=0
+            while num_processes<=64:
+                if mode == "mpi" or mode == "all":
+                    run_mpi(num_processes, input_file, place, log_folder)
+                    file_count+=2
 
-            if mode == "omp" or mode == "all":
-                run_omp(num_processes, input_file, log_folder)
+                if mode == "omp" or mode == "all":
+                    run_omp(num_processes, input_file, place, log_folder)
+                    file_count+=2
                 
-        num_processes*=2
+                sleep(2)
+                    
+            num_processes*=2
 
-        wait_completition(log_folder)
+            wait_completition(log_folder, file_count)
  
-
     run_serial(input_file)   
 
 if __name__ == "__main__":

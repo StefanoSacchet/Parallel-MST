5f163f7be06f2d2dc591e7bd3b34150b1ad6af08
luiss07 <luigi.delleva@gmail.com>
2025-06-04 09:59:12 +0200

    minor run serial

--- run_serial.sh
+++ run_serial.sh
@@ -2,21 +2,22 @@
 
 # Check if input file is passed
 if [ $# -lt 1 ]; then
-    echo "Usage: $0 <input_file> <optional_log_folder>"
+    echo "Usage: $0 <input_file> <output_name> <optional_log_folder>"
     exit 1
 fi
 
 input_file="$1"
+output_name="$2"
 log_folder="logs"
 
-if [ -z "$2" ]; then
+if [ -z "$3" ]; then
     log_folder="logs/"
     echo "Using default log folder: '$log_folder'"
 else
     # Remove any trailing slash from log_folder
     log_folder="${log_folder%/}"
     # Remove any leading/trailing slashes from input
-    input="${2#/}"
+    input="${3#/}"
     input="${input%/}"
     
     log_folder="$log_folder/$input/"
@@ -35,12 +36,13 @@ job_script=$(mktemp)
 
 cat <<EOF > "$job_script"
 #!/bin/bash
-#PBS -l select=1:ncpus=1:mem=64gb
-#PBS -l walltime=00:20:00
+#PBS -l select=1:ncpus=1:mem=128gb
+#PBS -l place=pack:excl
+#PBS -l walltime=01:00:00
 #PBS -q short_cpuQ
-#PBS -N serial_mst
-#PBS -o ${log_folder}serial_parallel_mst.o1
-#PBS -e ${log_folder}serial_parallel_mst.e1
+#PBS -N ser_${output_name}
+#PBS -o ${log_folder}serial_${output_name}.o1
+#PBS -e ${log_folder}serial_${output_name}.e1
 ${PWD}/build/bin/serial_mst "$input_file"
 EOF
 
